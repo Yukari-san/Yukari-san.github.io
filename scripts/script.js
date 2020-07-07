c2f604b67463988a4ff7f0881f9d811643311de6
Yukari-san <57048776+Yukari-san@users.noreply.github.com>
2020-07-07 16:17:23 -0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,7 @@
-.
+function changeBackground (background) {
+  document.body.style.backgroundImage = background;
+}
+
+function changeInfo (url) {
+  document.getElementById('iframe-pinfo').src = url;
+}
